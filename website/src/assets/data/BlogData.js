a6af693d557c47309ccf4871de027ef42e61a5a7
takumi765 <iwsktkm19@gmail.com>
2023-08-17 14:33:56 +0900

    add blog

--- website/src/assets/data/BlogData.js
+++ website/src/assets/data/BlogData.js
@@ -1,6 +1,11 @@
 /* ブログの情報 */
 const blogInfoList_RAW = [
   {
+    date : 20230817,
+    type : "hatena",
+    url  : "https://challenge-think.hatenablog.com/entry/2023/08/17/142807",
+  },
+  {
     date : 20230808,
     type : "hatena",
     url  : "https://challenge-think.hatenablog.com/entry/2023/08/08/114215",
