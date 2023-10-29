4dbe00c7c235a1192cf0dd0f3eca406acab3a4fe
Takumi765 <82143606+takumi765@users.noreply.github.com>
2023-10-29 22:01:24 +0900

    add app

--- website/src/assets/data/AppsData.js
+++ website/src/assets/data/AppsData.js
@@ -1,9 +1,16 @@
 const appInfoList_RAW = [
   {
-    type   : "便利",
+    type   : "遊び",
+    title  : "カスタム麻雀",
+    detail : "ブラウザで麻雀が出来ます",
+    appUrl : `<a href="https://github.com/senskids/CustomMahjong" target=_blank>こちら</a>`,
+    blogUrl: ``,
+  },
+  {
+    type   : "勉強",
     title  : "復習テストアプリ",
     detail : "テストを登録・修正・削除をすることができます。登録済みテストを科目ごとにランダムに出題し苦手な問題を割り出すことが出来ます。",
-    appUrl : `<a href="http://54.248.134.52/login/" target=_blank>こちら</a>`,
+    appUrl : `<a href="http://54.248.134.52/login/" target=_blank>こちら</a>※サイト停止`,
     blogUrl: `<a href="https://note.com/good_echium475/m/m6ef56a7ba8c2" target=_blank>こちら</a>`,
   },
 ]
